813e537d8941f96e278744fffa8b52eca0402952
Todd Wolden <tcwolden@gmail.com>
2022-11-08 09:28:04 -0700

    images working

--- src/Main.js
+++ src/Main.js
@@ -8,15 +8,15 @@ class Main extends React.Component {
       <>
         <main>
           <HornedBeast
-            title="Elk in Velvet Horns"
-            imageUrl ="./src/images/elk.jpg"
-            description="Image of Elk in Velvet Horns at Rocky Mountain National Park"
+            title="Unicorn Dancing in a Track Suit"
+            imageUrl ="https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg"
+            description="Image of a Dancing Unicorn"
           >
           </HornedBeast>
           <HornedBeast
-            title="American Bison"
-            imageUrl="./src/bison.jpg"
-            description="Image of American Bison at Yellowstone National Park"
+            title="Rhinoceros and Two Babies"
+            imageUrl="https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80"
+            description="Image of a Rhinoceros Family"
           >
           </HornedBeast>
         </main>
