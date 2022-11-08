2a8d742eb8fd9d10b1f1446f4112bb67a8a26417
Todd Wolden <tcwolden@gmail.com>
2022-11-08 16:26:20 -0700

    handle likes handle clicks

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -3,27 +3,33 @@ import './App.css';
 // import Button from 'react-bootstrap/Button';
 
 class HornedBeast extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     likes: 0
-  //   };
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      likes: 0,
+      clicks: 0
+    };
+  }
 
-  // handleLikes = () => {
-  //   //increment the number of likes by one
-  //   this.setState({
-  //     likes: this.state.likes + 1
-  //   });
-  // };
+  handleLikes = () => {
+    this.setState({
+      likes: this.state.likes + 1
+    });
+  };
+
+  handleClicks = () => {
+    this.setState({
+      clicks: this.state.clicks + 1
+    });
+  }
 
   render() {
     return (
       <>
-
+        
         <h2>{this.props.title}</h2>
-        {/* <p onClick={this.handleLikes}></p> */}
-        {/* <p>{this.state.likes} likes.</p> */}
+        {<p onClick={this.handleLikes}></p>}
+        {<p>{this.state.likes} likes.</p>}
         <img
           src={this.props.imageUrl}
           alt={this.props.description}
