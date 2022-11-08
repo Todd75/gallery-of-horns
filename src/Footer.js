a9c05729579304e571c8870ea2ef8003b281f083
Todd Wolden <tcwolden@gmail.com>
2022-11-07 21:27:59 -0700

    header and footer work

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return (
+        <footer>
+          <h2>Todd Wolden</h2>
+        </footer>
+    )
+  }
+
+}
+
+export default Footer;
