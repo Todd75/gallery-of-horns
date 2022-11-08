b2108cee216378da3fdaae36afb871dd811a769f
Todd Wolden <tcwolden@gmail.com>
2022-11-07 22:01:43 -0700

    working on hornedbeast.js

--- src/Main.js
+++ src/Main.js
@@ -1,1 +1,27 @@
 import React from 'react';
+import HornedBeast from './HornedBeast.js';
+import './App.css';
+
+class Main extends React.Component {
+  render() {
+    return (
+      <>
+        <main>
+          <HornedBeast
+            title=""
+            imageUrl=""
+            description=""
+          >
+          </HornedBeast>
+          <HornedBeast
+            title=""
+            imageUrl=""
+            description=""
+          >
+          </HornedBeast>
+        </main>
+      </>
+    )
+  }
+}
+export default Main;
