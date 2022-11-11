94318f11c7f8b7b30b02185b4277af4afa5da619
Todd Wolden <tcwolden@gmail.com>
2022-11-11 09:03:55 -0700

    faded in main page cards over 6 seconds

--- src/SelectedBeast.js
+++ src/SelectedBeast.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import './App.css';
-import './SelectedBeasts.css';
+
 
 class SelectedBeast extends React.Component {
     render() {
